aaf4acb5ad8e47d8c8fef51e9c7b37145528e9ce
Fanny Desjardins <123126813+FannyDesjardins@users.noreply.github.com>
2023-11-11 16:07:52 -0500

    Add files via upload

---
+++ Mobs/Reef1/Fish1.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+var SPEED= 50
+var player
+var chase = false
+
+#func _ready():
+	#get_node("AnimatedSprite2D").play("Idle")
+
+
+func _physics_process(delta): # Called every frame. 'delta' is the elapsed time since the previous frame.
+	if chase == true:
+		#get_node("AnimatedSprite2D").play("Jump")
+		player = get_node("../../Player/Player")
+		var direction = (player.position - self.position).normalized()
+		if direction.x > 0:
+			get_node("AnimatedSprite2D").flip_h = true;
+		else: 
+			get_node("AnimatedSprite2D").flip_h = false;
+		velocity.x = direction.x * SPEED
+	else:
+		#get_node("AnimatedSprite2D").play("Idle")
+		velocity.x = 0
+	move_and_slide()
+
+func _on_player_detection_body_entered(body):
+	if body.name == "Player":
+		chase = true;
+
+
+
+func _on_player_detection_body_exited(body):
+	if body.name == "Player":
+		chase = false;
